c22d7895ccc3b89d895761995908b3c320cce477
Jonathan <lafathan@gmail.com>
2025-12-27 16:35:29 -0500

    Added initial huffman tree building function.

--- internal/codec/huffman.go
+++ internal/codec/huffman.go
@@ -3,13 +3,14 @@ package codec
 type HUFFMANCodec struct{}
 
 type HuffmanNode struct {
-	nodeType  int                  // 0 is leaf, 1 is a node
-	value     byte                 // value held by a leaf
-	frequency int                  // frequency of value, or sum of frequencies of children
-	children  map[int]*HuffmanNode // children if not a leaf
+	nodeType  int // 0 is leaf, 1 is a node
+	parent    *HuffmanNode
+	value     byte            // value held by a leaf
+	frequency int             // frequency of value, or sum of frequencies of children
+	children  [2]*HuffmanNode // children if not a leaf
 }
 
-func GetHuffmanLeaves(src []byte) map[byte]*HuffmanNode {
+func GetHuffmanLeaves(src []byte) []*HuffmanNode {
 	freqMap := map[byte]*HuffmanNode{}
 	for _, b := range src {
 		_, ok := freqMap[b]
@@ -20,12 +21,44 @@ func GetHuffmanLeaves(src []byte) map[byte]*HuffmanNode {
 				frequency: 0,
 			}
 		}
-		freqMap[b].frequency = freqMap[b].frequency + 1
+		freqMap[b].frequency++
 	}
-	return freqMap
+	leaves := make([]*HuffmanNode, len(freqMap))
+	i := 0
+	for _, v := range freqMap {
+		leaves[i] = v
+		i += 1
+	}
+	return leaves
+}
+
+func TreeFromLeaves(leaves []*HuffmanNode) *HuffmanNode {
+	for len(leaves) > 1 {
+		leftChildIndex, rightChildIndex := 1, 0
+		for i, node := range leaves {
+			if node.frequency < leaves[leftChildIndex].frequency {
+				rightChildIndex, leftChildIndex = leftChildIndex, i
+			} else if node.frequency < leaves[rightChildIndex].frequency {
+				rightChildIndex = i
+			}
+		}
+		newNode := HuffmanNode{
+			nodeType:  1,
+			frequency: leaves[leftChildIndex].frequency + leaves[rightChildIndex].frequency,
+			children:  [2]*HuffmanNode{leaves[leftChildIndex], leaves[rightChildIndex]},
+		}
+		leaves[leftChildIndex].parent = &newNode
+		leaves[rightChildIndex].parent = &newNode
+		leaves[leftChildIndex] = &newNode
+		leaves = append(leaves[:rightChildIndex], leaves[rightChildIndex+1:]...)
+	}
+	return leaves[0]
 }
 
 func (HUFFMANCodec) EncodeBlock(src []byte) ([]byte, uint8, error) {
+	unsortedLeaves := GetHuffmanLeaves(src)
+	huffmanTree := TreeFromLeaves(unsortedLeaves)
+	print(huffmanTree)
 	return src, 0, nil
 }
 
