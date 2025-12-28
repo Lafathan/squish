969f6c7139fab1f7ed437818c650b5476f3b4e6b
Jonathan <lafathan@gmail.com>
2025-12-27 22:52:06 -0500

    Added huffman tree building from list of leaves.

--- internal/codec/huffman.go
+++ internal/codec/huffman.go
@@ -1,5 +1,10 @@
 package codec
 
+const (
+	Leaf   = 0
+	Branch = 1
+)
+
 type HUFFMANCodec struct{}
 
 type HuffmanNode struct {
@@ -11,46 +16,56 @@ type HuffmanNode struct {
 }
 
 func GetHuffmanLeaves(src []byte) []*HuffmanNode {
-	freqMap := map[byte]*HuffmanNode{}
-	for _, b := range src {
-		_, ok := freqMap[b]
-		if !ok {
+	if len(src) == 0 {
+		return nil
+	}
+	freqMap := map[byte]*HuffmanNode{} // build a dictionary of byte frequencies
+	for _, b := range src {            // loop through bytes
+		_, ok := freqMap[b] // check for existence
+		if !ok {            // create it if it doesn't exist
 			freqMap[b] = &HuffmanNode{
-				nodeType:  0,
+				nodeType:  Leaf,
 				value:     b,
 				frequency: 0,
 			}
 		}
-		freqMap[b].frequency++
+		freqMap[b].frequency++ // increment the frequency
 	}
-	leaves := make([]*HuffmanNode, len(freqMap))
+	leaves := make([]*HuffmanNode, len(freqMap)) // make a list of leaves
 	i := 0
 	for _, v := range freqMap {
 		leaves[i] = v
-		i += 1
+		i++
 	}
-	return leaves
+	return leaves // return the list
 }
 
 func TreeFromLeaves(leaves []*HuffmanNode) *HuffmanNode {
-	for len(leaves) > 1 {
-		leftChildIndex, rightChildIndex := 1, 0
-		for i, node := range leaves {
+	for len(leaves) > 1 { // while you are not at the root...
+		leftChildIndex := 0
+		for i, node := range leaves { // loop through the nodes picking the smallest frequency leaf
 			if node.frequency < leaves[leftChildIndex].frequency {
-				rightChildIndex, leftChildIndex = leftChildIndex, i
-			} else if node.frequency < leaves[rightChildIndex].frequency {
-				rightChildIndex = i
+				leftChildIndex = i
 			}
 		}
-		newNode := HuffmanNode{
-			nodeType:  1,
+		rightChildIndex := (leftChildIndex + 1) % len(leaves) // now do it for the right child
+		for i, node := range leaves {
+			if node.frequency < leaves[rightChildIndex].frequency && i != leftChildIndex {
+				rightChildIndex = i // pick the right child ensuring it is unique to the left
+			}
+		}
+		newNode := HuffmanNode{ // build a new node containing the two children
+			nodeType:  Branch,
 			frequency: leaves[leftChildIndex].frequency + leaves[rightChildIndex].frequency,
 			children:  [2]*HuffmanNode{leaves[leftChildIndex], leaves[rightChildIndex]},
 		}
 		leaves[leftChildIndex].parent = &newNode
 		leaves[rightChildIndex].parent = &newNode
-		leaves[leftChildIndex] = &newNode
-		leaves = append(leaves[:rightChildIndex], leaves[rightChildIndex+1:]...)
+		maxIndex := max(leftChildIndex, rightChildIndex)           // pick the rightmost index
+		leaves = append(leaves[:maxIndex], leaves[maxIndex+1:]...) // delete it
+		minIndex := min(leftChildIndex, rightChildIndex)           // pick the leftmost index
+		leaves = append(leaves[:minIndex], leaves[minIndex+1:]...) // delete it
+		leaves = append(leaves, &newNode)                          // append the new node you created
 	}
 	return leaves[0]
 }
