dc15a7defa9eeaf41130d112d13500e579049e4d
Jonathan <lafathan@gmail.com>
2026-01-14 21:53:24 -0500

    Small tweak to how printing help works.

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -2,6 +2,7 @@ package cli
 
 import (
 	"fmt"
+	"os"
 )
 
 func Run(args []string) int {
@@ -14,8 +15,6 @@ func Run(args []string) int {
 		return runEnc(args[1:])
 	case "dec":
 		return runDec(args[1:])
-	//case "analyze":
-	//	return runAnalyze(args[1:])
 	default:
 		fmt.Printf("unknown command: %q\n\n", args[0])
 		printMainHelp()
@@ -24,37 +23,35 @@ func Run(args []string) int {
 }
 
 func printMainHelp() {
-	fmt.Printf(`
-squish - a simple compression and decompression utility
-
-USAGE:
-	squish <command> [flags] [input]
-
-COMMANDS:
-	enc     Compress input into a .sqz stream
-	dec     Decompress a .sqz stream into original bytes
-	analyze Show information about a .sqz stream (codecs, blocks, ratios, etc.)
-
-INPUT/OUTPUT:
- 	input defaults to '-' (stdin) if omitted
-	output defauls to '-' (stdout) if -o/--output is not provided
-	use '-' to explicitly mean stdin/stdout
-
-FLAGS:
-  -o, -output <path|->
-        output file (defaults to '-')
-  -v, -version
-        display current version of squish
-  -h, -help
-        show help and exit 
-
-Examples:
-	squish enc -codec RLE-HUFFMAN -o ./output.sqz ./input.txt 
-	squish enc -codec RLE --blocksize 256KiB -o ./out.sqz -
-	squish dec -o ./input.txt ./output.sqz 
-	squish dec -o - ./compressed.sqz 
-	squish analyze ./output.sqz
-
-Run 'squish <command> -h' for command specific help.
-`)
+	fmt.Fprintf(os.Stdout, "squish - a simple compression and decompression utility\n")
+	fmt.Fprintf(os.Stdout, "\n")
+	fmt.Fprintf(os.Stdout, "USAGE:\n")
+	fmt.Fprintf(os.Stdout, "squish <command> [flags] [input]\n")
+	fmt.Fprintf(os.Stdout, "\n")
+	fmt.Fprintf(os.Stdout, "COMMANDS:\n")
+	fmt.Fprintf(os.Stdout, "enc     Compress input into a .sqz stream\n")
+	fmt.Fprintf(os.Stdout, "dec     Decompress a .sqz stream into original bytes\n")
+	fmt.Fprintf(os.Stdout, "analyze Show information about a .sqz stream (codecs, blocks, ratios, etc.)\n")
+	fmt.Fprintf(os.Stdout, "\n")
+	fmt.Fprintf(os.Stdout, "INPUT/OUTPUT:\n")
+	fmt.Fprintf(os.Stdout, "input defaults to '-' (stdin) if omitted\n")
+	fmt.Fprintf(os.Stdout, "output defauls to '-' (stdout) if -o/--output is not provided\n")
+	fmt.Fprintf(os.Stdout, "use '-' to explicitly mean stdin/stdout\n")
+	fmt.Fprintf(os.Stdout, "\n")
+	fmt.Fprintf(os.Stdout, "FLAGS:\n")
+	fmt.Fprintf(os.Stdout, "-o, -output <path|->\n")
+	fmt.Fprintf(os.Stdout, "output file (defaults to '-')\n")
+	fmt.Fprintf(os.Stdout, "-v, -version\n")
+	fmt.Fprintf(os.Stdout, "display current version of squish\n")
+	fmt.Fprintf(os.Stdout, "-h, -help\n")
+	fmt.Fprintf(os.Stdout, "show help and exit \n")
+	fmt.Fprintf(os.Stdout, "\n")
+	fmt.Fprintf(os.Stdout, "Examples:\n")
+	fmt.Fprintf(os.Stdout, "squish enc -codec RLE-HUFFMAN -o ./output.sqz ./input.txt \n")
+	fmt.Fprintf(os.Stdout, "squish enc -codec RLE --blocksize 256KiB -o ./out.sqz -\n")
+	fmt.Fprintf(os.Stdout, "squish dec -o ./input.txt ./output.sqz \n")
+	fmt.Fprintf(os.Stdout, "squish dec -o - ./compressed.sqz \n")
+	fmt.Fprintf(os.Stdout, "squish analyze ./output.sqz\n")
+	fmt.Fprintf(os.Stdout, "\n")
+	fmt.Fprintf(os.Stdout, "Run 'squish <command> -h' for command specific help.\n")
 }
