dc4c4b058ddac11ad0e54de4ccc341ef10291659
Jonathan <lafathan@gmail.com>
2026-02-13 21:58:57 -0500

    Swapped to uvarint for zero runs in zrle.

--- internal/codec/zrle.go
+++ internal/codec/zrle.go
@@ -1,8 +1,6 @@
 package codec
 
-const (
-	maxZeroRunLength uint8 = 255
-)
+import "encoding/binary"
 
 type ZRLECodec struct {
 	byteLength int
@@ -14,24 +12,27 @@ func (ZRLECodec) EncodeBlock(src []byte) ([]byte, error) {
 		return src, nil
 	}
 	var (
-		runLen   uint8  = 0                         // length of current run
+		runLen   uint64 = 0                         // length of current run
 		srcIdx   int    = 0                         // index as you traverse the source
 		outBytes []byte = make([]byte, 0, len(src)) // encoded bytes
 	)
 	for srcIdx < len(src) {
-		if src[srcIdx] == 0x00 && runLen < maxZeroRunLength {
+		if src[srcIdx] == 0x00 {
 			if runLen == 0 {
 				outBytes = append(outBytes, 0x00)
 			}
 			runLen++
 		} else {
 			if runLen > 0 {
-				outBytes = append(outBytes, runLen)
+				outBytes = binary.AppendUvarint(outBytes, runLen)
 				runLen = 0
 			}
 			outBytes = append(outBytes, src[srcIdx])
 		}
 		srcIdx++
+	}
+	if runLen > 0 {
+		outBytes = binary.AppendUvarint(outBytes, runLen)
 	}
 	return outBytes, nil
 }
@@ -41,13 +42,16 @@ func (ZRLECodec) DecodeBlock(src []byte) ([]byte, error) {
 		return src, nil
 	}
 	var (
-		srcIdx    = 0 // where you are in the source
-		outLength = 0 // first pass variable for allocating for decoding
+		srcIdx           = 0 // where you are in the source
+		outLength uint64 = 0 // first pass variable for allocating for decoding
+		run       uint64
+		bytes     int
 	)
 	for srcIdx < len(src) {
 		if src[srcIdx] == 0x00 {
-			srcIdx++
-			outLength += int(src[srcIdx])
+			run, bytes = binary.Uvarint(src[srcIdx+1:])
+			outLength += run
+			srcIdx += bytes
 		}
 		outLength++
 		srcIdx++
@@ -56,10 +60,11 @@ func (ZRLECodec) DecodeBlock(src []byte) ([]byte, error) {
 	outBytes := make([]byte, 0, outLength)
 	for srcIdx < len(src) {
 		if src[srcIdx] == 0x00 {
-			srcIdx++
-			for range src[srcIdx] {
+			run, bytes = binary.Uvarint(src[srcIdx+1:])
+			for range run {
 				outBytes = append(outBytes, 0x00)
 			}
+			srcIdx += bytes
 		} else {
 			outBytes = append(outBytes, src[srcIdx])
 		}
