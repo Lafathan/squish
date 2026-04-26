496b3642ab29c4d4476696777f6ed05e843d58fa
Jonathan <lafathan@gmail.com>
2026-04-25 23:00:23 -0400

    Moved to array copying for MTF. Copying saw better performance and removed requirement for allocating.

--- internal/codec/mtf.go
+++ internal/codec/mtf.go
@@ -2,65 +2,49 @@ package codec
 
 type MTFCodec struct{}
 
-func makeAlphabet() ([256]byte, [256]byte) {
-	// make an alphabet along with a positional reference array
-	var sym, pos [256]byte
+func makeAlphabet() [256]byte {
+	// create a symbol alphabet
+	var sym [256]byte
 	for i := range len(sym) {
-		j := byte(i)
-		sym[i] = j
-		pos[j] = j
+		sym[i] = byte(i)
 	}
-	return sym, pos
+	return sym
 }
 
 func (MTFCodec) EncodeBlock(src []byte) ([]byte, error) {
-	// encode src using the move-to-front transform
-	sym, pos := makeAlphabet()
-	// loop through the source performing the mtf transform
-	for sIdx := range len(src) {
-		// get the value and replace it with the index of where it is in the alphabet
-		sSym := src[sIdx]
-		aIdx := pos[sSym]
-		src[sIdx] = aIdx
-		// nothing else to do if it is already in the front
-		if aIdx == 0 {
-			continue
+	// make the symbol alphabet
+	symbols := makeAlphabet()
+	// loop through the input
+	for i, v := range src {
+		// if the data value matches the symbol
+		for j, s := range symbols {
+			// replace it with the index in the alphabet
+			if s == v {
+				src[i] = byte(j)
+				copy(symbols[1:], symbols[:j])
+				symbols[0] = s
+				break
+			}
 		}
-		// otherwise shift all other symbols to the right along with their alphabet indexes
-		for j := aIdx; j > 0; j-- {
-			x := sym[j-1]
-			sym[j] = x
-			pos[x] = j
-		}
-		// put the current symbol up front
-		sym[0] = sSym
-		pos[sSym] = 0
 	}
 	return src, nil
 }
 
 func (MTFCodec) DecodeBlock(src []byte) ([]byte, error) {
-	// decode src using the inverse move-to-front transform
-	sym, pos := makeAlphabet()
-	// loop through the source performing the inverse mtf transform
-	for sIdx := range len(src) {
-		// get the index and replace it with the value at that index in the alphabet
-		aIdx := src[sIdx]
-		sSym := sym[aIdx]
-		src[sIdx] = sSym
-		// nothing else to do if it is already in the front
-		if aIdx == 0 {
-			continue
+	// make the symbol alphabet
+	symbols := makeAlphabet()
+	// loop through the input
+	for i, v := range src {
+		// if the data value matches the symbol
+		for j, s := range symbols {
+			// replace it with the value at that index
+			if byte(j) == v {
+				src[i] = s
+				copy(symbols[1:], symbols[:j])
+				symbols[0] = s
+				break
+			}
 		}
-		// otherwise shift all other symbols to the right along with their alphabet indexes
-		for j := aIdx; j > 0; j-- {
-			x := sym[j-1]
-			sym[j] = x
-			pos[x] = j
-		}
-		// put the current symbol up front
-		sym[0] = sSym
-		pos[sSym] = 0
 	}
 	return src, nil
 }
